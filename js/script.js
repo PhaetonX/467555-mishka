4dcf4ca84782e0744354f485fe8385ef99c2ebc1
PhaetonX <anton-kuznecov@mail.ru>
2017-10-10 00:20:44 +0300

    Исправление

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ var siteMenu = document.querySelector('.page-header__site-menu');
 var userMenu = document.querySelector('.page-header__user-menu');
 
 toggle.addEventListener("click", function(event) {
-	toggle.classList.toggle("page-header__toggle--closed");
-	if (toggle.classList.contains('page-header__toggle--closed')) {
-		siteMenu.classList.remove('page-header__site-menu--closed');
-		userMenu.classList.remove('page-header__user-menu--closed');
-	} else {
-		siteMenu.classList.add('page-header__site-menu--closed');
-		userMenu.classList.add('page-header__user-menu--closed');
-	}
+  toggle.classList.toggle("page-header__toggle--closed");
+  if (toggle.classList.contains('page-header__toggle--closed')) {
+  siteMenu.classList.remove('page-header__site-menu--closed');
+  userMenu.classList.remove('page-header__user-menu--closed');
+  } else {
+  siteMenu.classList.add('page-header__site-menu--closed');
+  userMenu.classList.add('page-header__user-menu--closed');
+  }
 });
