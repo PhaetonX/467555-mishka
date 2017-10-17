32f8ccfbd022c3592df3ffa934de0ca19a4ad71b
PhaetonX <anton-kuznecov@mail.ru>
2017-10-17 16:49:40 +0300

    Разметка

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ var toggle = document.querySelector('.page-header__toggle');
 var siteMenu = document.querySelector('.page-header__site-menu');
 var userMenu = document.querySelector('.page-header__user-menu');
 
-toggle.addEventListener("click", function(event) {
-  toggle.classList.toggle("page-header__toggle--closed");
+siteMenu.classList.remove('page-header__site-menu--no-js');
+userMenu.classList.remove('page-header__user-menu--no-js');
+toggle.classList.remove('page-header__toggle--no-js;');
+
+toggle.addEventListener('click', function(event) {
+  toggle.classList.toggle('page-header__toggle--closed');
   if (toggle.classList.contains('page-header__toggle--closed')) {
     siteMenu.classList.remove('page-header__site-menu--closed');
     userMenu.classList.remove('page-header__user-menu--closed');
@@ -17,20 +21,20 @@ toggle.addEventListener("click", function(event) {
 //modal window
 
 var order = document.querySelector('.week-product__order');
-var modal = document.querySelector('.modal');
+var modal = document.querySelector('.modal-order');
 var addToBasket = document.querySelector('.product-preview__add-to-basket');
 
 if (order !== null) {
-  order.addEventListener("click", function(event) {
+  order.addEventListener('click', function(event) {
     event.preventDefault();
-    modal.classList.add("modal-show");
+    modal.classList.add('modal-show');
   });
 }
 
 if (addToBasket !== null) {
-  addToBasket.addEventListener("click", function(event) {
+  addToBasket.addEventListener('click', function(event) {
     event.preventDefault();
-    modal.classList.add("modal-show");
+    modal.classList.add('modal-show');
   });
 }
 
